b26431943a007464a849df1a1c6b9cb0277e4540
Fake098 <nikhilrathore846@gmail.com>
2024-09-05 10:04:39 +0530

    some changes

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 def fetch_video_info(url):
     ydl_opts = {
-        'quiet': True,
+       'quiet': True,
         'format': 'bestvideo+bestaudio/best',
+        'headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        },
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
